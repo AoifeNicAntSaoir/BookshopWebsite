1b231d232bff83a7aaa08867a68ee69e47107a36
Aoife Sayers <aoifesayers@gmail.com>
2018-01-30 14:54:59 +0000

    Delete TryConn.php

--- TryConn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbcnx = mysqli_connect("sql8.freesqldatabase.com", "sql8126604", "DHjsYWPBpf", "sql8126604"); 
-//checkconnection
-if (mysqli_connect_errno($dbcnx ))
-{echo "Failed to connect to MySQL: " .mysqli_connect_error();
-exit();
-}
-
-?>
